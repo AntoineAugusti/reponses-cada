a9a738ab2319af2ca9135e73486a6b2c16eddd0b
Antoine Augusti <antoine.augusti@gmail.com>
2019-11-17 21:36:56 +0100

    Keep only avis

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv("cada-2019-11-17.csv")
-df = df.loc[df.Année > 2015, :]
+df = df.loc[df.Type == "Avis"]
+df = df.loc[df.Année > 2015]
 
 by_admin = df.Administration.value_counts()
 by_admin = by_admin.loc[by_admin > 10]
@@ -16,6 +17,6 @@ final = pd.concat(
     [pct.round(2).rename("pourcentage_reponse"), by_admin.rename("nb_demandes")],
     axis=1,
     sort=False,
-).sort_values(by="nb_demandes")
+).sort_values(by="nb_demandes", ascending=False)
 final.index.name = "administration"
 final.to_csv("data.csv")
